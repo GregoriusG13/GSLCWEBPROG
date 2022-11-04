22734430a4bc4cee4174239a303af37baef957de
Gregorius Gilang Threesulistianto <68765622+GregoriusG13@users.noreply.github.com>
2022-11-04 14:29:01 +0700

    Add files via upload

---
+++ storage/framework/views/a56aa0d112734ebbe28641dcf53031c36c814286.php
@@ -0,0 +1,17 @@
+<?php echo $__env->make('navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php $__env->startSection('title','home'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="jumbotron">
+<div class="p-3 mb-2 bg-primary text-white">
+    <h1 class="display-4">Tugas GSLC5!</h1>
+    <p class="lead">Buatlah suatu tampilan website menggunakan framework laravel feature blade sebagai berikut:</p>
+    <hr class="my-4">
+    <p>berikut adalah tampilan web yang telah saya buat.</p>
+</div>
+  </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gregorius gilang\Desktop\Kampus\.punya gilang\Materi\S5\WebProg\Assignment\tugas5\resources\views/home.blade.php ENDPATH**/ ?>

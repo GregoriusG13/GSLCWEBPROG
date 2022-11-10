029bddb3bc26bdb4b989d2a265f9b433c09a15b2
Gregorius Gilang Threesulistianto <68765622+GregoriusG13@users.noreply.github.com>
2022-11-10 16:56:43 +0700

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@include('navbar')
+
+@extends('template')
+
+@section('title','home')
+
+@section('content')
+<div class="jumbotron">
+<div class="p-3 mb-2 bg-primary text-white">
+    <h1 class="display-4">Tugas GSLC6 Update controller!</h1>
+    <p class="lead">Tugas view,routes, dan controller</p>
+    <hr class="my-4">
+    <p>berikut adalah tampilan web yang telah saya buat.</p>
+</div>
+  </div>
+@endsection
